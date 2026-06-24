bb0395c9cac49f4b11c855156afd7664842fe6c8
InkaPrajapati <inkaprajapati54@gmail.com>
2026-06-24 22:40:57 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-</div> <!-- end content wrapper -->
-
-<!-- ================= FOOTER ================= -->
-<footer class="footer">
-    <p>© <?php echo date("Y"); ?> Online Exam Portal | All Rights Reserved</p>
-</footer>
-
-</body>
-</html>
